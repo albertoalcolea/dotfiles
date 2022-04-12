366bdc2f0409464fea998e018fb06c4711143f70
Alberto Alcolea <albertoalcolea@gmail.com>
2022-04-12 20:01:16 +0200

    Fixed alert alias. Gnome 42 does not show notifications with urgency=low, they appear as a dot next to the time

--- bash/.bash/aliases.sh
+++ bash/.bash/aliases.sh
@@ -20,7 +20,7 @@ alias l='ls -C'
 
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+alias alert='notify-send -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history | tail -n 1 | sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 # Activate Python virtual environment
 alias venv='source venv/bin/activate'
