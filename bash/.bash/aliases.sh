09c25da5409a96daa7cc29ff74d325856ec0079c
Alberto Alcolea <albertoalcolea@gmail.com>
2022-04-12 21:12:11 +0200

    Added more aliases to colorize the output of commands

--- bash/.bash/aliases.sh
+++ bash/.bash/aliases.sh
@@ -1,4 +1,4 @@
-# Enable color support of ls and greep and also add handy aliases
+# Enable color support of some commands
 if [ -x /usr/bin/dircolors ]; then
 	test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
 	alias ls='ls --color=auto'
@@ -8,6 +8,9 @@ if [ -x /usr/bin/dircolors ]; then
 	alias grep='grep --color=auto'
 	alias fgrep='fgrep --color=auto'
 	alias egrep='egrep --color=auto'
+
+	alias diff='diff --color=auto'
+	alias ip='ip -color=auto'
 fi
 
 # Some more ls aliases
