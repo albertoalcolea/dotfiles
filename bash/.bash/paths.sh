4e9cbfffc9d944bf4cbefc447e488a998c06b064
Alberto Alcolea <albertoalcolea@gmail.com>
2024-09-28 14:33:37 +0200

    work changes

--- bash/.bash/paths.sh
+++ bash/.bash/paths.sh
@@ -8,6 +8,10 @@ export DOTFILES_HOME="$(readlink -f "$(dirname ${BASH_SOURCE[0]})/../../")"
 # Python jupyter
 export JUPYTER_CONFIG_DIR=$XDG_CONFIG_HOME/jupyter
 
+# Ruby gems
+export GEM_HOME=$HOME/.local/lib/ruby/gems
+export PATH=$PATH:$HOME/.local/lib/ruby/gems/bin
+
 # Ruby bundler
 export BUNDLE_USER_CONFIG=$XDG_CONFIG_HOME/bundle
 export BUNDLE_USER_CACHE=$XDG_CACHE_HOME/bundle
